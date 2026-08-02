7f1926a2d08e7424b60a397d09bbf520b9a3348a
Milena Amaral <amaralmillena76@gmail.com>
2026-08-02 17:03:50 -0300

    feat: adiciona estrutura do temporizador

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
+
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('./sons/luna-rise-part-one.mp3');
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
 musica.loop = true;
 
 musicaFocoInput.addEventListener('change', () => {
@@ -58,3 +64,28 @@ function alterarContexto(contexto) {
         }
 }   
 
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        zerar();
+        alert("O tempo acabou!");
+        return
+    }       
+
+    tempoDecorridoEmSegundos -= 1;
+    console.log("Temporizador: " + tempoDecorridoEmSegundos);
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        zerar();
+        return;
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar() {
+    clearInterval(intervaloId);
+    intervaloId = null;
+}
