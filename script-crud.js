4be5733e7838dd2ce57188d8b3b9a1ca5e00c211
Milena Amaral <amaralmillena76@gmail.com>
2026-08-07 18:44:47 -0300

    feat: desselecionando uma tarefa

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,7 @@ const ulTarefas = document.querySelector(".app__section-task-list");
 const paragrafoDescricaoTarefa = document.querySelector(".app__section-active-task-description");
 
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+let tarefaSelecionada = null;
 
 function atualizarLocalStorage() {
     localStorage.setItem("tarefas", JSON.stringify(tarefas));
@@ -49,8 +50,21 @@ function criarElementoTarefa(tarefa) {
     li.append(botao);
 
     li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+            .forEach(elemento => {
+                elemento.classList.remove('app__section-task-list-item-active');
+            });
+        if (tarefaSelecionada === tarefa) {
+            paragrafoDescricaoTarefa.textContent = "";
+            tarefaSelecionada = null;
+            li.classList.remove('app__section-task-list-item-active');
+            return;
+        }   
+
+        tarefaSelecionada = tarefa;
         paragrafoDescricaoTarefa.textContent = tarefa.descricao;
-        li.classList.add('app__section-task-list-item-active')
+
+        li.classList.add('app__section-task-list-item-active');
     }   
 
     return li;
