fe550f47da06c8e8cd8d7cf92f3881a9a44ee386
Milena Amaral <amaralmillena76@gmail.com>
2026-08-07 19:06:28 -0300

    feat: adiciona finalizar de tarefa conforme tempo

--- script-crud.js
+++ script-crud.js
@@ -6,6 +6,7 @@ const paragrafoDescricaoTarefa = document.querySelector(".app__section-active-ta
 
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
 
 function atualizarLocalStorage() {
     localStorage.setItem("tarefas", JSON.stringify(tarefas));
@@ -57,11 +58,13 @@ function criarElementoTarefa(tarefa) {
         if (tarefaSelecionada === tarefa) {
             paragrafoDescricaoTarefa.textContent = "";
             tarefaSelecionada = null;
+            liTarefaSelecionada = null;
             li.classList.remove('app__section-task-list-item-active');
             return;
         }   
 
         tarefaSelecionada = tarefa;
+        liTarefaSelecionada = li;
         paragrafoDescricaoTarefa.textContent = tarefa.descricao;
 
         li.classList.add('app__section-task-list-item-active');
@@ -93,3 +96,11 @@ tarefas.forEach((tarefa) => {
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
 });  
+
+document.addEventListener('focoFinalizado', () => {
+    if (tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active');
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete');
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled');
+    }
+})
