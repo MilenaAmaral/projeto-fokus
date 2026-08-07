4e48b48502cb25e1b385446cdf7ed9ce009e086d
Milena Amaral <amaralmillena76@gmail.com>
2026-08-07 18:30:01 -0300

    feat: Adiciona selecionador de uma tarefa

--- script-crud.js
+++ script-crud.js
@@ -2,6 +2,7 @@ const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textarea = document.querySelector(".app__form-textarea");
 const ulTarefas = document.querySelector(".app__section-task-list");
+const paragrafoDescricaoTarefa = document.querySelector(".app__section-active-task-description");
 
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 
@@ -46,6 +47,11 @@ function criarElementoTarefa(tarefa) {
     li.append(svg);
     li.append(paragrafo);
     li.append(botao);
+
+    li.onclick = () => {
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+        li.classList.add('app__section-task-list-item-active')
+    }   
 
     return li;
 }
